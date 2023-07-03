6812d61499d222d1904438672c429947e45af15a
mikhaelapg <mikhaelapg00@gmail.com>
2023-07-03 19:32:54 +0700

    membuat business logic

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -5,4 +5,25 @@ public class AplikasiTodolist {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Menampilkan todo list
+     */
+    public  static void showTodoList(){
+
+    }
+
+    /**
+     * Menambah todo list
+     */
+    public static void addTodoList(String todo){
+
+    }
+
+    /**
+     * Menghapus todo dari list
+     */
+    public static boolean removeTodoList(Integer number) {
+        return true;
+    }
 }
